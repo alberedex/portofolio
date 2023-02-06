05546332d2e9efa5e3937db3f542bc747d072349
Alberto Redondo <2001rs@gmail.com>
2023-02-07 00:22:41 +0100

    fix problems and clear code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 let p = document.getElementById("lenguajes__title_id");
-
-for (let i = 0; i < 4; i++) {
-    let newChild = p.firstChild.cloneNode(false);
-    p.appendChild(newChild);
-}
+let classs = document.getElementsByClassName("lenguajes__title_div");
+console.log(classs);
+Array.from(classs).forEach(element => {
+    let elementChild = element.firstElementChild;
+    console.log(elementChild);
+    console.log(element);
+    for (let i = 0; i < 10; i++) {
+        let newChild = elementChild.cloneNode(true);
+        element.appendChild(newChild);
+    }
+});
