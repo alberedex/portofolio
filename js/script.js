3090e5cae4a204dbe90718960137c64e8200acdf
Alberto Redondo <2001rs@gmail.com>
2023-02-08 14:48:23 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let p = document.getElementById("lenguajes__title_id");
 let classs = document.getElementsByClassName("lenguajes__title_div");
-console.log(classs);
+
+
 Array.from(classs).forEach(element => {
     let elementChild = element.firstElementChild;
     console.log(elementChild);
@@ -11,6 +12,56 @@ Array.from(classs).forEach(element => {
     }
 });
 
-$("#offcanvasNavbar2 a").click(function(){
+$("#offcanvasNavbar2 a").click(function () {
     $('.offcanvas').offcanvas('hide');
 });
+
+let html = $('p,a');
+let background = $('body');
+let nav = $('nav');
+let navOffCanvas = $('nav div.offcanvas');
+let selector = $('.form-switch i');
+
+selector.click(onModeDarkLight);
+
+function onModeDarkLight() {
+    if(selector.hasClass('fa-regular fa-moon')){
+        darkMode();
+    } else {
+        lightMode();
+    }
+}
+
+onModeDarkLight();
+
+function darkMode() {
+    selector.removeClass();
+    selector.addClass('fa-solid fa-sun');
+
+    html.css('filter', 'invert(0%)');
+    selector.css('color', 'white');
+    background.css('background-color', 'black');
+    nav.addClass('bg-black');
+    nav.removeClass('bg-white');
+    
+
+    navOffCanvas.addClass('bg-black');
+    navOffCanvas.removeClass('bg-white');
+
+
+}
+
+function lightMode() {
+    selector.removeClass();
+
+    selector.addClass('fa-regular fa-moon');
+    html.css('filter', 'invert(100%)');
+    selector.css('color', 'black');
+    background.css('background-color', 'white');
+
+    nav.removeClass('bg-black');
+    nav.addClass('bg-white');
+
+    navOffCanvas.addClass('bg-white');
+    navOffCanvas.removeClass('bg-black');
+}
